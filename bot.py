c3db02de56859dd7c1b73779ca2b5eec9e4be469
Дмитрий Спиряев <imwisagist@yandex.ru>
2023-03-20 11:55:45 +0300

    Ðv1.7

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ import aiopg
 import requests
 from aiogram import Bot, Dispatcher, executor, types
 from bs4 import BeautifulSoup
+from requests import Response
+
 import config as cfg
 
 dp = Dispatcher(Bot(token=cfg.TELEGRAM_TOKEN))
@@ -16,7 +18,6 @@ async def get_data_from_db(sql_query: str) -> list:
         async with connection.cursor() as cursor:
             await cursor.execute(sql_query)
             data = await cursor.fetchall()
-
         pool.close()
 
     return data
@@ -50,12 +51,80 @@ async def parse_contest(source_contest: list) -> list:
     return parsed_tasks
 
 
-async def get_task_descriptions(task_url: str) -> str:
-    response = requests.get(task_url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+class Chapter:
+    def __init__(self, task: BeautifulSoup, class_: str, section: str) -> None:
+        self.output = self.value = self.description = self.header = None
+        self.task = task
+        self.class_ = class_
+        self.activate_fields(section)
 
-    description = soup.find('div', {'class': 'problem-statement'})
-    return description.text.strip()
+    def activate_fields(self, title) -> None:
+        if title == 'header':
+            self.activate_header_fields()
+        elif title == 'input':
+            self.activate_input_fields()
+
+    def activate_input_fields(self) -> None:
+        self.description = self.task.find('div', {'class': f'{self.class_}'})
+        self.value = self.description.next_sibling.text.strip()
+        self.description = self.description.text.strip()
+        self.output = self.description + '\n' + self.value
+
+    def activate_header_fields(self) -> None:
+        self.class_ = self.task.find('div', {'class': f'{self.class_}'})
+        self.description = self.class_.find(
+            'div', {'class': 'property-title'}).text.strip()
+        self.value = str(self.class_.find(
+            'div', {'class': 'property-title'}).next_sibling)
+        self.output = self.description + ': ' + self.value
+
+
+async def get_task_descriptions(task_url: str) -> str:
+    response: Response = requests.get(task_url)
+    soup: BeautifulSoup = BeautifulSoup(response.text, 'html.parser')
+
+    task: BeautifulSoup = soup.find('div', {'class': 'problem-statement'})
+
+    header: BeautifulSoup = soup.find('div', {'class': 'header'})
+    title: str = header.find('div', {'class': 'title'}).text.strip()
+    time_limit: Chapter = Chapter(task, 'time-limit', 'header')
+    memory_limit: Chapter = Chapter(task, 'memory-limit', 'header')
+    file_input: Chapter = Chapter(task, 'input-file', 'header')
+    file_output: Chapter = Chapter(header, 'output-file', 'header')
+
+    task_description: str = soup.find(
+        'div', {'class': 'header'}).next_sibling.text.strip()
+
+    input_: BeautifulSoup = soup.find('div', {'class': 'input-specification'})
+    input_specification: Chapter = Chapter(input_, 'section-title', 'input')
+
+    result = f"""
+{title}\n{'--'*len(title)}
+{time_limit.output}
+{memory_limit.output}
+{file_input.output}
+{file_output.output}
+{'--'*25}
+{task_description}
+{'--'*25}
+{input_specification.output}
+"""
+
+    return result
+
+
+@dp.message_handler(commands=['task'])
+async def print_task_description(message: types.Message):
+    task_id = int(message.text.split()[-1])
+    name_number_index = await get_data_from_db(
+        f"SELECT name_and_number FROM tasks WHERE id={task_id}"
+    )
+
+    num, idx = name_number_index[-1][-1][-1].split('/')
+    await message.answer(
+        await get_task_descriptions(
+            f"https://codeforces.com/problemset/problem/{num}/{idx}?locale=ru")
+    )
 
 
 @dp.message_handler(commands=['tags'])
@@ -74,6 +143,22 @@ async def print_ratings_for_tag(message: types.Message):
         f"SELECT DISTINCT rating FROM contests WHERE tag='{tag}'"
     )
     await message.answer(', '.join(map(str, await parse_db_response(source))))
+
+
+@dp.message_handler(commands=['contest'])
+async def print_tasks_from_define_contest(message: types.Message):
+    contest_id = int(message.text.split()[-1])
+    source_contest = await get_data_from_db(
+        f"SELECT tasks from contests Where id={contest_id}"
+    )
+    [await message.answer("""
+Идентификатор - {},
+Темы - ({}),
+Решено раз - {},
+Название - "{}", 
+Номер и индекс - {},
+Сложность - {},
+""".format(*task)) for task in await parse_contest(source_contest)]
 
 
 @dp.message_handler(commands=['contests'])
@@ -97,35 +182,6 @@ async def print_contests_for_tag_and_rating(message: types.Message):
 Номер контеста - {},
 Тема - {},
 Сложность - {}""".format(*contest))
-
-
-@dp.message_handler(commands=['contest'])
-async def print_tasks_from_define_contest(message: types.Message):
-    contest_id = int(message.text.split()[-1])
-    source_contest = await get_data_from_db(
-        f"SELECT tasks from contests Where id={contest_id}"
-    )
-    [await message.answer("""
-Идентификатор - {},
-Темы - ({}),
-Решено раз - {},
-Название - "{}", 
-Номер и индекс - {},
-Сложность - {},
-""".format(*task)) for task in await parse_contest(source_contest)]
-
-
-@dp.message_handler(commands=['task'])
-async def print_task_description(message: types.Message):
-    task_id = int(message.text.split()[-1])
-    name_number_index = await get_data_from_db(
-        f"SELECT name_and_number FROM tasks WHERE id={task_id}"
-    )
-
-    number, index = name_number_index[-1][-1][-1].split('/')
-    await message.answer(
-        await get_task_descriptions(f"https://codeforces.com/problemset/problem/{number}/{index}?locale=ru")
-    )
 
 
 @dp.message_handler(commands=['help'])
